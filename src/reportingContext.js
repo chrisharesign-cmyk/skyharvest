c8cc9b2c60fdacdc2ffbdfb78477ef71b83aa365
chrisharesign-cmyk <chris.haresign@gmail.com>
2026-05-26 22:39:59 +0100

    feat: add periodLabel to reporting context

--- src/reportingContext.js
+++ src/reportingContext.js
@@ -1,6 +1,12 @@
 import { createContext, useContext } from "react";
 
-export const ReportingSheetsContext = createContext({ sheets: [], setSheets: () => {} });
+export const ReportingSheetsContext = createContext({
+  sheets: [],
+  setSheets: () => {},
+  periodLabel: "All data",
+  setPeriodLabel: () => {},
+});
+
 export function useReportingSheets() {
   return useContext(ReportingSheetsContext);
 }
